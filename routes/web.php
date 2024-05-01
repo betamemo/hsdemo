b385440dd34194e42e9c7d86457d015666bfd115
betamemo <bandita.kul@gmail.com>
2024-05-01 18:19:11 +0700

    refactor routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,28 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Public Routes
+
 Route::get('/', \App\Http\Controllers\WelcomeController::class);
 
 Route::resource('authors', \App\Http\Controllers\AuthorController::class)->only('index', 'show');
 
 Route::resource('articles', \App\Http\Controllers\ArticleController::class);
+
+Route::get('articles', function () {
+    return view('articles');
+});
+
+// User Routes
+
+require __DIR__ . '/auth.php';
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
